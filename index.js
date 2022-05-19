67a94d8b534c4c33acd462e37063d602edfdd64f
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-19 17:21:55 -0400

    commit tree 98f48bd021dfb1a4367033d760c1bc93fcfe4d02

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"]
+
+function writeCards(array, event) {
+    let cards = []
+    for(let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+        
+    }
+    return cards
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer = integer - 1;
+    }
+}
